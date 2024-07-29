1fb77070a174ba682cdc6c8f2481477207f80667
negarbaharmand <negarbaharmand@outlook.com>
2024-07-29 10:00:51 +0200

    Add index.html and index.js files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
+const element = <h2 className="text-danger">Hello React!</h2>;
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(element);
